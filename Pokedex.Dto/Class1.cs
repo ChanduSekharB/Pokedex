0abcbff37aeb3a1bd8e22772f5924dae53059f65
Chandu <Chandu@DESKTOP-U21F4RP>
2021-05-21 22:22:53 +0530

    Created Project Structure.

---
+++ Pokedex.Dto/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Pokedex.Dto
+{
+    public class Class1
+    {
+    }
+}
---
+++ Pokedex.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Pokedex.Models
+{
+    public class Class1
+    {
+    }
+}
